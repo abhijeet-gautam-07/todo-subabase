e6170159bb89196f5455ad6960372f5ee0fe4390
Abhijeet <ajkin778@outlook.com>
2025-11-22 00:19:15 +0530

    added force-dynamic

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,6 +2,8 @@ import { AuthCard } from "@/components/auth/auth-card";
 import { LoginForm } from "@/components/auth/login-form";
 import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 export default async function LoginPage() {
   const session = await getSession();
